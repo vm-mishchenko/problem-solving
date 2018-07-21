c2bd6ad9b58e7cb9780744b248d6e042a91f037b
vm-mishchenko <vm.mishchenko@gmail.com>
2018-07-21 17:06:59 +0300

    journey-the-the-moon

---
+++ src/hacker-rank/jorney-to-the-moon.js
@@ -0,0 +1,11 @@
+// https://www.hackerrank.com/challenges/journey-to-the-moon/problem
+
+function journeyToMoon(n, astronaut) {
+
+}
+
+describe('Hacker Rank: Journey to the Moon', function () {
+    it('should work', function () {
+        expect(journeyToMoon(5, [[0, 1], [2, 3], [0, 4]])).toBe(6);
+    });
+});
